cd775ee9ee86c51f4e7639896d508c39b13015d0
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2022-03-18 17:26:02 +0100

    wip

--- faim_dl/data_record/DataRecord.py
+++ faim_dl/data_record/DataRecord.py
@@ -1,14 +1,12 @@
 import json
-from os.path import join, splitext, basename, dirname, split, exists
+from os.path import join, dirname, split, exists
 
 import numpy as np
-import pandas as pd
 import zarr
 from ome_zarr.io import parse_url
 from ome_zarr.scale import Scaler
 from ome_zarr.writer import write_image
 from skimage.io import imread
-import numcodecs
 
 import sqlite3 as db
 
@@ -113,11 +111,14 @@ class DefaultDataRecord(DataRecord):
                         data_spacing,
                         target_spacing,
                         min_shape,
-                        dtype,
-                        compressor,
-                        train,
-                        val,
+                        source_dtype,
+                        target_dtype,
                         target_name='nuclei'):
+        assert source_dtype in [np.uint8, np.int8, np.int16, np.int32,
+                                        np.int64, np.float64, np.float32, np.float16, np.complex64, np.complex128, bool]
+        assert target_dtype in [np.uint8, np.int8, np.int16, np.int32,
+                                        np.int64, np.float64, np.float32, np.float16, np.complex64, np.complex128, bool]
+
         if name in list(container.group_keys()):
             root = container[name]
         else:
@@ -125,49 +126,141 @@ class DefaultDataRecord(DataRecord):
             root.attrs['name'] = name
             root.attrs['type'] = 'ome-zarr'
 
-        offset = len(root)
-        if np.all(target_spacing == data_spacing):
-            factors = (1,) * len(target_spacing)
-        else:
-            factors = target_spacing / data_spacing
+        with db.connect(self.source_files_db) as conn:
+            c = conn.cursor()
+            c.execute(
+                "CREATE TABLE IF NOT EXISTS {} ('source_file' text NOT NULL, 'target_file' text NOT NULL, 'index' INT NOT NULL)".format(
+                    name)
+            )
 
-        for i, (src_f, trg_f) in enumerate(zip(source_files, target_files)):
-            grp = root.create_group(str(offset + i))
-            labels_grp = grp.create_group('labels')
-            label_grp = labels_grp.create_group(target_name)
+            index = len(root)
+            if np.all(target_spacing == data_spacing):
+                factors = np.array((1,) * len(target_spacing))
+            else:
+                factors = np.array(target_spacing) / np.array(data_spacing)
 
-            src = imread(src_f).astype(dtype)
-            trg = imread(trg_f).astype(dtype)
+            for i, (src_f, trg_f) in enumerate(zip(source_files, target_files)):
 
-            if np.any(factors != 1):
-                src = rescale(src, scale=factors, mode='reflect', anti_aliasing=True, preserve_range=True,
-                              order=1).astype(src.dtype)
-                trg = self.rescale_labeling(trg, factors=factors).astype(trg.dtype)
+                assert exists(src_f), "File {} could not be found.".format(src_f)
+                assert exists(trg_f), "File {} could not be found.".format(trg_f)
 
-            if np.any(src.shape < min_shape):
-                src = np.pad()
+                src_file_exists = DefaultDataRecord.file_in_db(c, src_f)
+                trg_file_exists = DefaultDataRecord.file_in_db(c, trg_f)
 
-            write_image(image=src, group=grp, chunks=chunks, axes=axes, scaler=Scaler(downscale=1, max_layer=0))
-            write_image(image=trg, group=label_grp, chunks=chunks, axes=axes,
-                        scaler=Scaler(downscale=1, max_layer=0))
+                if not src_file_exists and not trg_file_exists:
+                    grp = root.create_group(str(index))
+                    labels_grp = grp.create_group('labels')
+                    label_grp = labels_grp.create_group(target_name)
+                    self.add_image(axes, chunks, data_spacing, source_dtype, factors, grp, min_shape, src_f,
+                                   DefaultDataRecord.mi_ma_fun)
+                    if target_dtype in [np.uint8, np.int8, np.int16, np.int32,
+                                        np.int64]:
+                        self.add_label_image(axes, chunks, data_spacing, target_dtype, factors, label_grp, min_shape,
+                                             trg_f)
+                    else:
+                        self.add_image(axes, chunks, data_spacing, target_dtype, factors, label_grp, min_shape, trg_f,
+                                       DefaultDataRecord.mi_ma_fun)
 
-            grp.attrs['dataspecs'] = {
-                "source_file": src_f,
-                "target_file": trg_f,
-                "shape": src.shape,
-                "axes": axes,
-                "data_spacing": data_spacing
-            }
+                    c.execute("INSERT INTO {} (source_file, target_file, [index]) VALUES ('{}', '{}', {})".format(name, src_f, trg_f, index))
+                    index += 1
 
-        self.attachments[name] = join(".", name)
+            self.attachments[name] = join(".", name)
 
-        return pd.DataFrame({"source_file": source_files, "target_file": target_files})
+    def add_image(self, axes, chunks, data_spacing, dtype, factors, grp, min_shape, file, mi_ma_fun):
+        img = imread(file).astype(dtype)
+
+        img = DefaultDataRecord.normalize_min_max(img, *mi_ma_fun(img))
+
+        if np.any(factors != 1):
+            img = rescale(img, scale=factors, mode='reflect', anti_aliasing=True, preserve_range=True,
+                          order=1).astype(img.dtype)
+
+        img = self.pad(img, min_shape, mode='reflect')
+
+        if "c" not in axes:
+            img = img[np.newaxis]
+            axes = ("c", ) + axes
+            chunks = (1, ) + chunks
+            data_spacing = (1, ) + data_spacing
+            factors = np.concatenate([np.array([1]), factors])
+
+        write_image(image=img, group=grp, chunks=chunks, axes=axes, scaler=Scaler(downscale=1, max_layer=0))
+        grp.attrs['dataspecs'] = {
+            "source_file": file,
+            "shape": img.shape,
+            "axes": axes,
+            "data_spacing": tuple(np.array(data_spacing, np.float32) * factors)
+        }
+
+    def pad(self, img, min_shape, mode='reflect', constant_values=-1):
+        if np.any(img.shape < min_shape):
+            pad_width = []
+            for img_s, min_s in zip(img.shape, min_shape):
+                margin = min_s - img_s
+                pad_width.append([margin // 2, min_s - margin // 2])
+            img = np.pad(img, pad_width=pad_width, mode=mode, constant_values=constant_values)
+        return img
+
+    def add_label_image(self, axes, chunks, data_spacing, dtype, factors, grp, min_shape, file):
+        img = imread(file).astype(dtype)
+        if np.any(factors != 1):
+            img = self.rescale_labeling(img, factors=factors).astype(dtype)
+
+        img = self.pad(img, min_shape, mode='constant', constant_values=-1)
+
+        if "c" not in axes:
+            img = img[np.newaxis]
+            axes = ("c", ) + axes
+            chunks = (1, ) + chunks
+            data_spacing = (1, ) + data_spacing
+            factors = np.concatenate([np.array([1]), factors])
+
+        write_image(image=img, group=grp, chunks=chunks, axes=axes, scaler=Scaler(downscale=1, max_layer=0))
+        grp.attrs['dataspecs'] = {
+            "source_file": file,
+            "shape": img.shape,
+            "axes": axes,
+            "data_spacing": tuple(np.array(data_spacing, dtype=np.float32) * factors)
+        }
+
+    @staticmethod
+    def mi_ma_fun(img):
+        mi, ma = np.quantile(img, [0.03, 0.998])
+        return mi, ma
+
+    @staticmethod
+    def normalize_min_max(data, mi, ma):
+        return (data - mi) / (ma - mi)
+
+    @staticmethod
+    def file_in_db(c, file):
+        tables = [t[0] for t in c.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()]
+
+        for table in tables:
+            if DefaultDataRecord.source_file_exists(c, table, file) or DefaultDataRecord.target_file_exists(c, table,
+                                                                                                            file):
+                return True
+
+        return False
+
+    @staticmethod
+    def source_file_exists(c, table, file):
+        return bool(c.execute(
+            "SELECT COUNT(1) FROM '{}' WHERE source_file = '{}'".format(table, file)
+        ).fetchall()[0][0])
+
+    @staticmethod
+    def target_file_exists(c, table, file):
+        return bool(c.execute(
+            "SELECT COUNT(1) FROM '{}' WHERE target_file = '{}'".format(table, file)
+        ).fetchall()[0][0])
 
     def create_train_val_test_dataset(self, source_files, target_files, chunks, axes, data_spacing, target_spacing,
                                       min_shape,
                                       val_fraction,
                                       test_fration,
-                                      dtype=np.uint16, compressor=numcodecs.Blosc(), seed=42):
+                                      source_dtype=np.float32, target_dtype=np.uint16,
+                                      seed=42):
         assert len(source_files) == len(target_files)
         assert len(source_files) > 3
 
@@ -187,8 +280,8 @@ class DefaultDataRecord(DataRecord):
                             data_spacing=data_spacing,
                             target_spacing=target_spacing,
                             min_shape=min_shape,
-                            dtype=dtype,
-                            compressor=compressor)
+                            source_dtype=source_dtype,
+                            target_dtype=target_dtype)
 
         self.add_val_data(source_files=source_files[random_idx[n_train_samples:n_train_samples + n_val_samples]],
                           target_files=target_files[random_idx[n_train_samples:n_train_samples + n_val_samples]],
@@ -197,8 +290,8 @@ class DefaultDataRecord(DataRecord):
                           data_spacing=data_spacing,
                           target_spacing=target_spacing,
                           min_shape=min_shape,
-                          dtype=dtype,
-                          compressor=compressor)
+                          source_dtype=source_dtype,
+                          target_dtype=target_dtype)
 
         self.add_test_data(source_files=source_files[random_idx[-n_test_samples:]],
                            target_files=target_files[random_idx[-n_test_samples:]],
@@ -207,65 +300,47 @@ class DefaultDataRecord(DataRecord):
                            data_spacing=data_spacing,
                            target_spacing=target_spacing,
                            min_shape=min_shape,
-                           dtype=dtype,
-                           compressor=compressor)
-
-    def save2db(self, files, table_name):
-        with db.connect(self.source_files_db) as conn:
-            c = conn.cursor()
-            c.execute(
-                "CREATE TABLE IF NOT EXISTS {} ('source_file' text NOT NULL, 'target_file' text NOT NULL)".format(
-                    table_name)
-            )
-            files.to_sql(table_name, conn, if_exists="append", index=False)
+                           source_dtype=source_dtype,
+                           target_dtype=target_dtype)
 
     def add_train_data(self, source_files, target_files, chunks, axes, data_spacing, target_spacing, min_shape,
-                       dtype=np.uint16, compressor=numcodecs.Blosc()):
+                       source_dtype=np.float32, target_dtype=np.uint16):
         assert len(source_files) == len(target_files)
-        files = self._create_dataset(self.zarr_container, name="train_data",
-                                     source_files=source_files,
-                                     target_files=target_files,
-                                     chunks=chunks,
-                                     axes=axes,
-                                     data_spacing=data_spacing,
-                                     target_spacing=target_spacing,
-                                     min_shape=min_shape,
-                                     dtype=dtype,
-                                     compressor=compressor,
-                                     train=True, val=False)
-
-        self.save2db(files, table_name="train_files")
+        self._create_dataset(self.zarr_container, name="train_data",
+                             source_files=source_files,
+                             target_files=target_files,
+                             chunks=chunks,
+                             axes=axes,
+                             data_spacing=data_spacing,
+                             target_spacing=target_spacing,
+                             min_shape=min_shape,
+                             source_dtype=source_dtype,
+                             target_dtype=target_dtype)
 
     def add_val_data(self, source_files, target_files, chunks, axes, data_spacing, target_spacing, min_shape,
-                     dtype=np.uint16, compressor=numcodecs.Blosc()):
+                     source_dtype=np.float32, target_dtype=np.uint16):
         assert len(source_files) == len(target_files)
-        files = self._create_dataset(self.zarr_container, name="val_data",
-                                     source_files=source_files,
-                                     target_files=target_files,
-                                     chunks=chunks,
-                                     axes=axes,
-                                     data_spacing=data_spacing,
-                                     target_spacing=target_spacing,
-                                     min_shape=min_shape,
-                                     dtype=dtype,
-                                     compressor=compressor,
-                                     train=False, val=True)
-
-        self.save2db(files, table_name="val_files")
+        self._create_dataset(self.zarr_container, name="val_data",
+                             source_files=source_files,
+                             target_files=target_files,
+                             chunks=chunks,
+                             axes=axes,
+                             data_spacing=data_spacing,
+                             target_spacing=target_spacing,
+                             min_shape=min_shape,
+                             source_dtype=source_dtype,
+                             target_dtype=target_dtype)
 
     def add_test_data(self, source_files, target_files, chunks, axes, data_spacing, target_spacing, min_shape,
-                      dtype=np.uint16, compressor=numcodecs.Blosc()):
+                      source_dtype=np.float32, target_dtype=np.uint16):
         assert len(source_files) == len(target_files)
-        files = self._create_dataset(self.zarr_container, name="test_data",
-                                     source_files=source_files,
-                                     target_files=target_files,
-                                     chunks=chunks,
-                                     axes=axes,
-                                     data_spacing=data_spacing,
-                                     target_spacing=target_spacing,
-                                     min_shape=min_shape,
-                                     dtype=dtype,
-                                     compressor=compressor,
-                                     train=False, val=False)
-
-        self.save2db(files, table_name="test_files")
+        self._create_dataset(self.zarr_container, name="test_data",
+                             source_files=source_files,
+                             target_files=target_files,
+                             chunks=chunks,
+                             axes=axes,
+                             data_spacing=data_spacing,
+                             target_spacing=target_spacing,
+                             min_shape=min_shape,
+                             source_dtype=source_dtype,
+                             target_dtype=target_dtype)
